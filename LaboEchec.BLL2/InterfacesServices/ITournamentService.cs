34ecae369e2f5684dacf1271d98f1d3dc97a4dd8
Bryan kenis <bryan_kenis@hotmail.com>
2022-08-12 10:58:46 +0200

    Creation Tournament Fin

--- LaboEchec.BLL2/InterfacesServices/ITournamentService.cs
+++ LaboEchec.BLL2/InterfacesServices/ITournamentService.cs
@@ -11,6 +11,6 @@ namespace LaboEchec.BLL.InterfacesServices
 {
     public interface ITournamentService
     {
-        Tournament TournamentCreate(TournamentRegister newTournament);
+        Tournament TournamentCreate(TournamentService newTournament)
     }
 }
